25f2c234fdf3ef4ac30763fc5c10edf86600a1af
Macbook <macbook@macbook-pro-de-macbook.home>
2026-09-01 20:57:18 +0200

    modif video

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const mediaLibrary = {
       { type: 'image', src: 'photos/salon2.jpeg', alt: 'Appartement F5 Bambilor' },
       { type: 'image', src: 'photos/chambreF.jpeg' },
       { type: 'image', src: 'photos/Cuisine.jpeg', alt: 'Cuisine de l’appartement F5' },
-      { type: 'video', src: 'photos/video2.mp4', title: 'Vidéo de l’appartement F5' }
+      { type: 'video', src: 'photos/video3.mp4', title: 'Vidéo de l’appartement F5' }
     ]
   }
 };
